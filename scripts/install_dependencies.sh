73e1b371ddfb5f5d4cd561d9700cdbbe2e17e08d
Nick Maskell <nrmaskell@gmail.com>
2018-04-23 23:12:09 +1200

    Remove apt-get

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-
-# need to first somehow get the files to the right locations and ready to go.
-# something in here: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html
-
-sudo apt-get update # Update machine
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install --lts
