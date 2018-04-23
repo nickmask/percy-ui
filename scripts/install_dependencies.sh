ec18263383093d91c6a5a40ceec2f1f1cce2ac13
Nick Maskell <nrmaskell@gmail.com>
2018-04-23 23:06:58 +1200

    Add install node command

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,9 @@
 # something in here: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html
 
 sudo apt-get update # Update machine
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - sudo apt-get install -y nodejs # install node and npm
-cd percy-ui
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+
+cd ~/percy-ui
 npm install
